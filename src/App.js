5cc0a3790f4b382d1044bc8dfebecf3abba3c198
Islan Gomes <100229515+Lanmgomez@users.noreply.github.com>
2022-07-04 14:45:37 -0300

    Enviando atualizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
 import Footer from './components/Footer';
+/**BrowserRouter */
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import AboutUs from './components/pages/AboutUs';
 import Option from './components/pages/Option';
+/**Dark Theme */
+import {useState} from 'react'
+import styled, {ThemeProvider} from 'styled-components'
+import {lightTheme, darkTheme, GlobalStyles} from './components/pages/theme.js'
+
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor}
+`
 
 function App() {
+
+  const [theme, setTheme] = useState("light")
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light")
+  } 
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/aboutus' element={<AboutUs />}/>
-          <Route path='/option' element={<Option />}/>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        <button onClick={() => themeToggler()}>Change Theme</button>
+        <BrowserRouter>
+          <Navbar/>
+            <Routes>
+              <Route path='/' element={<Home />}/>
+              <Route path='/aboutus' element={<AboutUs />}/>
+              <Route path='/option' element={<Option />}/>
+            </Routes>
+        </BrowserRouter>
       <Footer />
+      </StyledApp>
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/pages/Option.js
+++ src/components/pages/Option.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Option = () => {
+
   return (
     <div>Option</div>
   )
